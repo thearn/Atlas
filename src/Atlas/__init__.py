18253d58c4901e8bc5aa1d6d6ca20f875810e84c
Tristan Hearn <tristan.a.hearn@nasa.gov>
2014-03-19 18:08:26 -0400

    finished vortex wake calculation

--- src/Atlas/__init__.py
+++ src/Atlas/__init__.py
@@ -1,4 +1,3 @@
 from coefficients import dragCoefficient, dragCoefficientFit, frictionCoefficient
 from properties import discretizeProperties, wireProperties, sparProperties, chordProperties
-from failure import failureCalc, torsionalBucklingFailure
-#from vortex import vortexWakeCover
+from vortex import vortexRing, inducedVelocity
